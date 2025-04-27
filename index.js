6eed14a1798b9f6d01cfa9f5db37292cb54d7343
sashpawar11 <saisaipawar@gmail.com>
2025-04-27 14:56:53 +0530

    urlparams

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 import http from 'http'
 import fs from 'fs'
-
+import url from 'url'
 
 const httpServer = http.createServer((req, res) => {
     
-    const log = `${Date.now().toLocaleString()} : New Request Received : ${req.method}`
+    const urlPath = url.parse(req.url, true);
+    console.log(urlPath);
+    
+
+    const log = `${Date.now().toLocaleString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+    })}: New Request Received : ${req.url}\n`
     
     fs.appendFile('logs.txt', log,(err, data) => {
 
-        switch (req.url) {
+        switch (urlPath.pathname) {
             case '/': res.end('Homepage');
                 break;
             case '/about': res.end('About');
+                break;
+            case '/contact': res.end(`Hello there, ${urlPath.query.name}, From: ${urlPath.query.state}`)
                 break;
             default: res.end('404 Page not Found');
         }
